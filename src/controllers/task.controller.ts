5c4a991ecd8ac776f897b059ba0da368e6375635
Amogh Deshpande <deshpandeamogh25@gmail.com>
2026-03-11 18:22:33 +0530

    added workspace ownership check

--- src/controllers/task.controller.ts
+++ src/controllers/task.controller.ts
@@ -1,12 +1,24 @@
 import { Response } from "express";
 import { AuthRequest } from "../types/auth.js";
 import { createTaskService } from "../services/task.service.js";
+import { prisma } from "../lib/prisma.js";
 
 export const createTask = async (req: AuthRequest, res: Response) => {
     const { title, description, projectId, assigneeId } = req.body;
 
     if (!title || !projectId) {
         return res.status(400).json({ message: "Missing fields" });
+    }
+
+    const project = await prisma.project.findFirst({
+        where: {
+            id: projectId,
+            workspaceId: req.user!.workspaceId,
+        },
+    });
+
+    if(!project) {
+        return res.status(403).json({message: "Forbidden project"});
     }
 
     const task = await createTaskService({
